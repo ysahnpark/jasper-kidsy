3b33c1dc9f282f78a136e1563cf3d3b0d033a2fd
Young Suk Ahn <ys.ahnpark@gmail.com>
2015-01-26 20:59:44 -0500

    Add MathQuiz module

---
+++ MathQuiz.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8-*-
+from semantic.numbers import NumberService
+import random
+#from client import app_utils
+import re
+
+WORDS = ["MATH"]
+
+PRIORITY = 3
+
+
+def get_a_question(min=0, max=10, op='+'):
+
+    num1 = random.randint(min, max)
+    num2 = random.randint(min, max)
+    answer = num1 + num2
+    question = (answer, num1, num2)
+
+    return question
+
+
+def handle(text, mic, profile):
+    """
+        Responds to user-input, typically speech text, with a summary of
+        the day's top news headlines, sending them to the user over email
+        if desired.
+        Arguments:
+        text -- user-input, typically transcribed speech
+        mic -- used to interact with the user (for both input and output)
+        profile -- contains information related to the user (e.g., phone
+                   number)
+    """
+    question = get_a_question()
+    mic.say("What is " + question[1] + " plus " + question[2])
+    response = mic.activeListen()
+
+    numservice = NumberService()
+    user_answer  = numservice.parse(response)
+    if user_answer == question[0]:
+        mic.say("You are right!")
+    else:
+        mic.say("Nah, that is incorrect!")
+
+
+def isValid(text):
+    """
+        Returns True if the input is related to the news.
+        Arguments:
+        text -- user-input, typically transcribed speech
+    """
+    return bool(re.search(r'\b(math)\b', text, re.IGNORECASE))
---
+++ tests/testtext2num.py
@@ -0,0 +1,23 @@
+__author__ = 'ysahn'
+
+from text2num import text2num
+from semantic.numbers import NumberService
+import unittest
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def test_get_a_story(self):
+        numservice = NumberService()
+        result  = numservice.parse("one hundred forty four")
+        print(result)
+
+        self.assertEqual( 1 , text2num("one"))
+        self.assertEqual( 12 , text2num("twelve"))
+        self.assertEqual( 72 , text2num("seventy two"))
+        self.assertEqual( 300 , text2num("three hundred"))
+        self.assertEqual( 1200 , text2num("twelve hundred"))
+        self.assertEqual( 12304 , text2num("twelve thousand three hundred four"))
+        self.assertEqual( 6000000 , text2num("six million"))
+        self.assertEqual( 6400005 , text2num("six million four hundred thousand five"))
+        self.assertEqual( 123456789012 , text2num("one hundred twenty three billion four hundred fifty six million seven hundred eighty nine thousand twelve"))
+        self.assertEqual( 4000000000000000000000000000000000 , text2num("four decillion"))
